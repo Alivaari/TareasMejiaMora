5de251d3391d02112b14dee92a44fcb291f93db0
Alivaari <99691339+Alivaari@users.noreply.github.com>
2022-02-18 16:37:25 -0600

    Corrección de errores
    Se corrigen los errores del archivo "Errores_Flake_8.py" que el flake8 podía identificar.

--- Tarea1/Errores_Flake_8.py
+++ Tarea1/Errores_Flake_8.py
@@ -1,3 +1,4 @@
-def ErroresFlake8 ():  # Esto es un comentario extendido para que el flake8 lo reconozca como error
-    var="a"
+def ErroresFlake8():  # Esto es un comentario extendido
+    # para que el flake8 lo reconozca como error
+    var = "a"
     return var
